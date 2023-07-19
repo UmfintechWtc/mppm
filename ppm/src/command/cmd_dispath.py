f95075d6090842ac06dab76a169fd5026ada9917
wangtianci <wangtianci@starcross.tech>
2023-07-19 09:15:01 +0800

    [dev]callback cmd function exec method

--- ppm/src/command/cmd_dispath.py
+++ ppm/src/command/cmd_dispath.py
@@ -12,7 +12,7 @@ def dispath(args):
 	if not check_pip_version():
 		sys.exit(2)
 	if cmd_mapping.get(args.sub_cmd):
-		cmd_mapping[args.sub_cmd](args)
+		cmd_mapping[args.sub_cmd](args).exec()
 	else:
 		print_colored("Please Usage: {} {} -h".format(APP_NAME, args.sub_cmd), "red")
 		sys.exit(2)
