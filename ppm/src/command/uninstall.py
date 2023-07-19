669996f0dc645939634977e7b792702a8c69d2bb
TianCiwang <13623650548@163.com>
2023-07-19 21:01:26 +0800

    [dev]add setup.py

--- ppm/src/command/uninstall.py
+++
@@ -1,3 +0,0 @@
-class UninstallCmd():
-	def __init__(self, args):
-		self.module_name = args.module
--- setup.py
+++ setup.py
@@ -2,47 +2,52 @@
 
 import os
 import sys
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def fread(fname):
-	filepath = os.path.join(os.path.dirname(__file__), fname)
-	with open(filepath, 'r') as fp:
-		return fp.read()
+    filepath = os.path.join(os.path.dirname(__file__), fname)
+    with open(filepath, 'r') as fp:
+        return fp.read()
 
 
 required = [
-	'pip',
-	'crayons',
-	'blindspin',
-	'requests',
-	'pick',
-	'configparser',
+    'pip',
+    'crayons',
+    'blindspin',
+    'requests',
+    'pick',
+    'configparser',
 ]
 
 setup(
-	name='ppm',
-	version='0.0.1',
-	description='PyPI Mirror and Package Manager',
-	long_description=fread('README.md'),
-	keywords='pypi,mirror',
-	url='https://gitee.com/TianCiwang/ppm',
-	author='TianCiwang',
-	author_email='13623650548@163.com',
-	license='MIT',
-	install_requires=required,
-	packages=['ppm'],
-	entry_points={
-		'console_scripts': [
-			'ppm=run:start'
-		],
-	},
-	classifiers=[
+    name='mppi',
+    version='1.0.1',
+    description='PyPI Mirror and Package Manager',
+    keywords='pypi,mirror,package',
+    url='https://gitee.com/TianCiwang/mppi',
+    long_description=fread('README.md'),
+    long_description_content_type='text/markdown',
+    author='TianCiwang',
+    author_email='13623650548@163.com',
+    license='MIT',
+    install_requires=required,
+    packages=["mppi"],
+    entry_points={
+        'console_scripts': [
+            'mppi=mppi.run:main'
+        ],
+    },
+    classifiers=[
 		'Development Status :: 4 - Beta',
 		'Intended Audience :: Developers',
 		'License :: OSI Approved :: MIT License',
 		'Programming Language :: Python',
-		'Programming Language :: Python :: >= 3.6',
+		'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
 
 	]
 )
