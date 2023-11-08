9f12cd26e7384cc7a8b2030cc6a9f068ea23c801
UmfintechWtc <31309558+UmfintechWtc@users.noreply.github.com>
2023-11-08 11:14:15 +0800

    force rewrite config when download sub_command use -y/yes args

--- mppm/src/command/download.py
+++ mppm/src/command/download.py
@@ -8,6 +8,9 @@ class DownloadCmd():
         self.args = args
 
     def exec(self, pip_path):
+        if self.args.yes:
+            verification_pypi_url()
+            
         if self.args.module:
             create_dir(self.args.module)
             download_pip_pkg_cmd = "{} download {} -d {}".format(pip_path, self.args.module, self.args.module)
@@ -15,6 +18,7 @@ class DownloadCmd():
             create_dir(ARG_DOWNLOAD_REQUIREMENT)
             download_pip_pkg_cmd = "{} download -r {} -d {}".format(pip_path, self.args.requirement,
                                                                     ARG_DOWNLOAD_REQUIREMENT)
+        
         cmd_result = exec_cmd(download_pip_pkg_cmd)
         if cmd_result is None or ignore_errors[self.args.sub_cmd] in cmd_result:
             print_colored(f"the module and dependencies have been downloaded. Please check the current path", "green")
